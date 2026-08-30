7aaaab9e535c6919be7ae3205c29f3a03a04e86f
Trafalgar Justice Law <37120431+btajallin@users.noreply.github.com>
2026-08-30 14:14:28 -0400

    Configure RetroFetch Android app

---
+++ RetroFetch/app/build.gradle.kts
@@ -0,0 +1,30 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.retrofetch.app"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.retrofetch.app"
+        minSdk = 30
+        targetSdk = 36
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+        }
+    }
+}
